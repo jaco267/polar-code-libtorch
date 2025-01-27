3f0b178b794d490fde3e065bbcbbe218a429e4c7
Etin Horng <87292294+jaco267@users.noreply.github.com>
2025-01-27 19:45:11 +0800

    d

--- sys_model/awgn_model.h
+++ sys_model/awgn_model.h
@@ -19,22 +19,11 @@ private:
   int n_bits_per_sym = 2; 
   int n_;  int k_;  
   float coderate;
-  //* ----solution 1:new -----------
-  // QamConstell* constell_;   
-  //* ----solution 2: unique----------  
-  unique_ptr<QamConstell> constell_;//* sol 2 unique pointer  
-    // https://www.youtube.com/watch?v=UOB7-B2MfwA
-    //* I prefer unique_ptr in this case , since we only have one constell_  (dont need copy)
-    //* so we dont need to use shared_ptr ref count overhead  
-  //* ---solution 3: shared------
-  // shared_ptr<QamConstell> constell_;
-  // Mapper* mapper_;   Demapper* demapper_; 
+  unique_ptr<QamConstell> constell_;   
   unique_ptr<Mapper> mapper_;
-  unique_ptr<Demapper> demapper_;
-  // BinarySource* binary_src_; 
+  unique_ptr<Demapper> demapper_; 
   unique_ptr<BinarySource> binary_src_;
   unique_ptr<AWGN> awgn_channel_;
-  // BinarySource binary_sss;//* this will call default constructor
   PolarEncoder encoder_;
   SC_Dec dec_;
 };
